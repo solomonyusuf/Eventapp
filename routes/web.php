281dcca0f2d83cbde472124337ad858769fcaf67
Solomon yusuf <olafire03@gmail.com>
2025-11-20 21:27:23 +0100

    fix deploy

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Livewire\Homepage;
 use App\Livewire\RegisterPage;
+use App\Models\Record;
 use Illuminate\Support\Facades\Route;
+
 
 
 
@@ -10,3 +12,20 @@ Route::get('/', Homepage::class)->name('home');
 Route::get('/register', RegisterPage::class)->name('register');
 Route::post('/register-form', [RegisterPage::class, 'submit'])
     ->name('register_form');
+Route::get('/api/records', function(){
+    $records = Record::all()->map(function($record) {
+            return [
+                'id'        => $record->id,
+                'full_name' => $record->meta_data['full_name'] ?? null,
+                'email'     => $record->meta_data['email'] ?? null,
+                'phone'     => $record->meta_data['phone'] ?? null,
+                'passport'  => $record->meta_data['passport'] ?? null,
+                'created_at'=> $record->created_at->toDateTimeString(),
+            ];
+        });
+
+        return response()->json([
+            'success' => true,
+            'data'    => $records,
+        ]);
+});
